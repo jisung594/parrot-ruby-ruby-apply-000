0f74480198aad192a92aa109b80f786864a3572f
jonathan choi <jisung594@gmail.com>
2018-10-20 01:47:15 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string)
+def parrot(string = "Squawk!")
   puts "Squawk!"
   return string
 end
 
-parrot("Squawk!")
+parrot(string)
